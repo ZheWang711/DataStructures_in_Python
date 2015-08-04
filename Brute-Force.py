4e1111dd5bafc474520fd6b0ba92bfbd23b85768
ZheWang711 <619899442@sjtu.edu.cn>
2015-08-04 09:55:00 +0800

    repeatedly permutation in dictionary order

--- Brute-Force.py
+++ Brute-Force.py
@@ -43,23 +43,28 @@ class Permutation:
         self._dictionary_order_print(A, 0)
 
     def _dictionary_order_print(self, A, current):
-        if current == len(A) - 1:
+        if current == len(A):
             print(A)
         else:
             for i in range(current, len(A)):
-                # the elements in the range of [0, current - 1] is fixed
-                # the first elements we need to consider about rearranging is at index [current]
-                tmp = A[i]
-                A.remove(tmp)
-                A.insert(current, tmp)  # current is the first position allowed to rearranging
-                self._dictionary_order_print(A, current + 1)
-                A.remove(tmp)
-                A.insert(i, tmp)
+                # the for loop need to iterate over A's values non-repeatedly and non-omittedly
+                # Thus recursive call if and only if A[i] is unequal with the element A[i - 1]
+                # Since sequence A is already sorted, the index of same values in sequence A
+                # must be distributed continuously
+                if i == current or A[i] != A[i - 1]:
+                    # the elements in the range of [0, current - 1] is fixed
+                    # the first elements we need to consider about rearranging is at index [current]
+                    tmp = A[i]
+                    del(A[i])           # A.remove(tmp) this only remove the first element whose value is tmp
+                    A.insert(current, tmp)  # current is the first position allowed to rearranging
+                    self._dictionary_order_print(A, current + 1)
+                    del(A[current])     # A.remove(tmp)
+                    A.insert(i, tmp)
+
 
 
 
 if __name__ == "__main__":
     sample = Permutation()
-    A = [1, 2, 3]
+    A = [1, 1, 1, 2]
     sample.dictionary_order_print(A)
-    print(A)
